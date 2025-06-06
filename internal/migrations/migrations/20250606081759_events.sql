38b9939948237ef8c2f9027027eec00c592a1a8f
velanac <vidmedia@gmail.com>
2025-06-06 10:31:18 +0200

    Events

---
+++ internal/migrations/migrations/20250606081759_events.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS events (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID,
+    patient_id UUID,
+    title TEXT NOT NULL,
+    start_time TIMESTAMPTZ NOT NULL,
+    end_time TIMESTAMPTZ NOT NULL,
+    type VARCHAR(50) NOT NULL
+)
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS events;
+-- +goose StatementEnd
